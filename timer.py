e037b354364bd469b6a19f2c90e13fccdf01255b
Мартин Јанев <150262130+MartinJ1st@users.noreply.github.com>
2025-03-28 00:29:11 +0100

    added timer for calculating execution time

---
+++ timer.py
@@ -0,0 +1,15 @@
+from time import perf_counter_ns
+from typing import Any
+
+
+def profiler(method):
+    def wrapper_method(*args: Any, **kwargs: Any) -> Any:
+        start = perf_counter_ns()
+        result = method(*args, **kwargs)
+        end = perf_counter_ns() - start
+        time_len = min(9, ((len(str(end)) - 1) // 3) * 3)
+        timeConv = {9: 'seconds', 6: 'milliseconds', 3: 'microseconds', 0: 'nanoseconds'}
+        print(f"Result: {result} - Time: {end / (10 ** time_len)} {timeConv[time_len]}")
+        return result
+
+    return wrapper_method
